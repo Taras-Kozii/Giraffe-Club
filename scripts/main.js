cf573a94520c0fae1a8a921bfb6e9ac6972814c3
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-04-10 14:46:33 +0000

    start

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,9 @@
 'use strict';
-const slider = document.querySelector('.slider');
-const sliderBlock = document.querySelector('.slider__content');
-const sliders = sliderBlock.querySelectorAll('.slider__post');
-const prevButton = document.getElementById('prev');
-const nexButton = document.getElementById('next');
+const swiper = new Swiper('.swiper', {
+  autoHeight: true,
 
-let active = 0;
-const slidersCounts = sliders.length - 1;
-
-
-document.addEventListener('DOMContentLoaded', e => {
-  setSliderHeight();
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
-
-function setSliderHeight() {
-  slider.style.height = `${document.querySelector('.slider__content').offsetHeight}px`;
-}
-
-prevButton.addEventListener('click', () => {
-  active = active - 1 >= 0 ? active - 1 : slidersCounts;
-  moveSlider();
-});
-
-nexButton.addEventListener('click', () => {
-  active = active + 1 <= slidersCounts ? active + 1 : 0;
-  moveSlider();
-});
-
-
-// let refreshInterval = setInterval(()=> {next.click()}, 5000);
-
-function moveSlider() {
-  sliderBlock.style.left = -sliders[active].offsetLeft + 'px';
-//    clearInterval(refreshInterval);
-//    refreshInterval = setInterval(()=> {next.click()}, 5000);
-}
-window.addEventListener('resize', () => {
-  // setSliderHeight();
-  moveSlider();
-})
