b2496300e2c64a9befa5ffa58085a245ec948ff4
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-04-11 13:14:10 +0000

    start

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,50 @@
 'use strict';
-const swiper = new Swiper('.swiper', {
-  autoHeight: true,
-
+const reviewsSlider = new Swiper('.swiper', {
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.slider__button-next',
+    prevEl: '.slider__button-prev',
+  },
+  autoHeight: true,
+  speed: 800,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
+  },
+});
+
+const teamSlider = new Swiper('.slider-team__content', {
+  navigation: {
+    nextEl: '.slider__button-next',
+    prevEl: '.slider__button-prev',
+  },
+  autoHeight: true,
+  speed: 800,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  autoplay: {
+    delay: 5000,
+    stopOnLastSlide: false,
+    disableOnInteraction: true,
   },
 });
